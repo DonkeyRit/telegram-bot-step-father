ae0eb548e67d67c1d47e15e0c3df8069837b6365
Dima Alekseev <alekseevda24@gmail.com>
2023-11-27 09:31:21 +0100

    [TFS-0001] Introduce EventHandler

--- src/telegram-api-native-library/telegram-api-native-library-api/src/main/java/com/github/donkeyrit/telegrambotstepfather/tdlib/events/interfaces/EventBus.java
+++ src/telegram-api-native-library/telegram-api-native-library-api/src/main/java/com/github/donkeyrit/telegrambotstepfather/tdlib/events/interfaces/EventBus.java
@@ -2,4 +2,6 @@ package com.github.donkeyrit.telegrambotstepfather.tdlib.events.interfaces;
 
 public interface EventBus<T, EType extends Enum<EType>> {
     void publish(Event<T, EType> event) throws InterruptedException;
+    void subscribe(EType eventType, EventHandler<T, EType> handler);
+    void unsubscribe(EType eventType, EventHandler<T, EType> handler);
 }
--- src/telegram-api-native-library/telegram-api-native-library-api/src/main/java/com/github/donkeyrit/telegrambotstepfather/tdlib/events/queues/SimpleEventBus.java
+++ src/telegram-api-native-library/telegram-api-native-library-api/src/main/java/com/github/donkeyrit/telegrambotstepfather/tdlib/events/queues/SimpleEventBus.java
@@ -1,22 +1,43 @@
 package com.github.donkeyrit.telegrambotstepfather.tdlib.events.queues;
 
+import com.github.donkeyrit.telegrambotstepfather.tdlib.events.interfaces.EventHandler;
 import com.github.donkeyrit.telegrambotstepfather.tdlib.events.interfaces.EventBus;
-
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-
 import com.github.donkeyrit.telegrambotstepfather.tdlib.events.interfaces.Event;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.BlockingQueue;
+import java.util.Map;
 
 public class SimpleEventBus<T, EType extends Enum<EType>> implements EventBus<T, EType> {
 
-    private BlockingQueue<Event<T, EType>> queue;
+    private final Map<EType, BlockingQueue<Event<T, EType>>> eventQueues;
+    private final Map<EType, CopyOnWriteArraySet<EventHandler<T, EType>>> subscribers;
 
     public SimpleEventBus() {
-        queue = new ArrayBlockingQueue<Event<T, EType>>(100);
+        eventQueues = new ConcurrentHashMap<>();
+        subscribers = new ConcurrentHashMap<>();
     }
 
     @Override
     public void publish(Event<T, EType> event) throws InterruptedException {
+        BlockingQueue<Event<T, EType>> queue = eventQueues
+            .computeIfAbsent(event.getEventType(), k -> new LinkedBlockingQueue<>());
         queue.put(event);
+    }
+
+    @Override
+    public void subscribe(EType eventType, EventHandler<T, EType> handler) {
+        CopyOnWriteArraySet<EventHandler<T, EType>> set = subscribers
+            .computeIfAbsent(eventType, k -> new CopyOnWriteArraySet<>());
+        set.add(handler);
+    }
+
+    @Override
+    public void unsubscribe(EType eventType, EventHandler<T, EType> handler) {
+        if (subscribers.containsKey(eventType)) {
+            subscribers.get(eventType).remove(handler);
+        }
     }
 }
